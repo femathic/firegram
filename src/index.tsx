f7f0f2785b3f6b831921784586880979ff2b803e
Oluwafemi Opanuga <femathic@gmail.com>
2020-09-30 12:34:25 +0100

    Fix performance issues

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/tailwind.css';
-import './styles/main.scss';
+// import './styles/main.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(

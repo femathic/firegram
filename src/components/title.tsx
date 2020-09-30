5f6f0929b390bc9fd4cc353509aa0b2da778e678
Oluwafemi Opanuga <femathic@gmail.com>
2020-09-30 11:17:29 +0100

    Fix Image quality issues

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -3,13 +3,11 @@ import React from 'react';
 const Title = () => (
   <div>
     <h1 className="text-red-200 font-bold m-2 ml-8">FireGram</h1>
-    {/* <h1 className="font-bold m-2 ml-8">FireGram</h1> */}
     <h2 className="text-center font-bold text-3xl mt-10 mb-4">
       Your Pictures
     </h2>
     <h3 className="text-center font-medium text-sm text-gray-600">
-      {/* <h3 className="flex justify-center font-medium text-sm"> */}
-      Keep your pictures here and make memories today.
+      Create picture albums, make memories and share with friends.
     </h3>
   </div>
 );

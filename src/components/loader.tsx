81f678c66ff8870db4e23c96db057361a8cd2a0d
femathic <femathic@gmail.com>
2021-01-01 21:35:42 +0100

    Add htaccess file

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,12 +1,12 @@
-// import React from 'react';
+import React from 'react';
 
-// const Loader = () => (
-//   <div className="w-full h-screen flex items-center justify-center">
-//     <div
-//       className="animate-spin ease-linear rounded-full border-8 border-t-8 border-gray-400 dark:border-gray-200 h-20 w-20"
-//       style={{ borderTopColor: '#FB5C00' }}
-//     />
-//   </div>
-// );
+const Loader = () => (
+  <div className="w-full h-screen flex items-center justify-center">
+    <div
+      className="animate-spin ease-linear rounded-full border-8 border-t-8 border-gray-400 dark:border-gray-200 h-20 w-20"
+      style={{ borderTopColor: '#FB5C00' }}
+    />
+  </div>
+);
 
-// export default Loader;
+export default Loader;
